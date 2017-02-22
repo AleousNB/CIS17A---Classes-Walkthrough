e30bdee0c62ce83b126bbfc2609bf08c3254bbe8
ncc <ncc>
2017-02-22 10:23:37 -0800

    Course class added

--- Classes Walkthrough/Main.cpp
+++ Classes Walkthrough/Main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include "Student.h"
 #include "Teacher.h"
+#include "Course.h"
 
 using namespace std;
 
 int main()
 {
 	Teacher james = Teacher("james");
-	cout << james.GetName() << endl;
+	Course cis17 = Course("CIS17A", ":Programming C++: Objects",james);
+	Student bob = Student("bob");
+	Student jame = Student("jame");
+
+	cis17.EnrollStudent(bob, 0);
+	cis17.EnrollStudent(jame, 1);
 
 	getchar();
 	return 0;
--- Classes Walkthrough/Student.h
+++ Classes Walkthrough/Student.h
@@ -6,6 +6,10 @@ class Student
 private:
 	std::string _name;
 public:
+	Student();
+	//This is a default constructer since the student array from the 
+	// course class requires all the students which there all none
+	
 	Student(std::string name);
 	~Student();
 
